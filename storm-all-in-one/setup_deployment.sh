8e7506e28348b7a10bd0b86e9dd725e12c3261ac
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2014-10-18 12:20:46 +0200

    StoRM deploymen test setup

---
+++ storm-all-in-one/setup_deployment.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -x
+trap "exit 1" TERM
+export TOP_PID=$$
+
+terminate() {
+      echo $1 && kill -s TERM $TOP_PID
+}
+
+[ -n "${JENKINS_SLAVE_PRIVATE_KEY}"] || terminate "JENKINS_SLAVE_PRIVATE_KEY not set."
+[ -n "${REPO_URL}" ] || terminate "REPO_URL not set."
+[ -n "${PLATFORM}" ] || terminate "PLATFORM not set."
+[ -n "${MODE}" ] || terminate "MODE not set."
+
+STORM_DEPLOYMENT_TEST_REPO=${STORM_DEPLOYMENT_TEST_REPO:-https://github.com/italiangrid/storm-deployment-test.git}
+STORM_DEPLOYMENT_TEST_BRANCH=${STORM_DEPLOYMENT_TEST_BRANCH:-master}
+SSH_OPTIONS="-q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=false -i $JENKINS_SLAVE_PRIVATE_KEY"
+
+cat << EOF > deploy_storm.sh
+dir=$(mktemp -d storm-deployment.XXXX)
+cd $dir
+git clone $STORM_DEPLOYMENT_TEST_REPO
+git checkout $STORM_DEPLOYMENT_TEST_BRANCH
+cd storm-deployment-test
+export STORM_REPO=${REPO_URL}_$(echo $PLATFORM | tr '[:upper:]' '[:lower:]').repo
+export PATH=$PATH:/sbin:/usr/sbin
+sh ./$MODE-deployment_$PLATFORM.sh
+EOF
+
+scp ${SSH_OPTIONS} deploy_storm.sh ${EC2_USER}@${MACHINE_HOSTNAME}:
+[ $? -ne 0 ] || terminate "Error sending deployment script to ${MACHINE_HOSTNAME}"
+
+ssh -tt ${SSH_OPTIONS} ${EC2_USER}@${MACHINE_HOSTNAME} "sudo sh deploy_storm.sh"
+[ $? -ne 0 ] || terminate "Deployment test ERROR"
+
+echo "Deployment test success."
